0b8fc517816e5419bc4ab2c1367dfbc48dae02be
opet <opet@Academico.edu>
2018-03-13 22:23:43 -0300

    Aula 4

--- source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
+++ source/ProgramacaoVisual/WebMvc/Controllers/PessoasController.cs
@@ -12,26 +12,43 @@ namespace WebMvc.Controllers
     {
         private static IList<Pessoa> pessoaList = new List<Pessoa>
         {
-            new Pessoa {Id = 1, Nome = "Joao"},
-            new Pessoa {Id = 2, Nome = "Carlos"},
-            new Pessoa {Id = 3, Nome = "Marcelo"}
+            new Pessoa { Id = 1, Nome = "Joao"},
+            new Pessoa { Id = 2, Nome = "Juao"},
+            new Pessoa { Id = 3, Nome = "John"}
         };
 
-        //Actions
+        // Actions
 
-        //localhost:5000/Pessoas
-        //localhost:5000/Pessoas/Index
-
+        // localhost:5000/Pessoas
+        // localhost:5000/Pessoas/Index
         public IActionResult Index()
         {
-            //busca dos dados
-            //transforma dados em dados de visualização
+            // busca dos dados
+            // transforma dados em dados de visualização
             var viewModel = new PessoaViewModel
             {
                 Items = pessoaList
             };
-            //visualização dos dados
+            // visualização dos dados
             return View(viewModel);
-        }     
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Nome")]Pessoa newPessoa)
+        {
+            if (!ModelState.IsValid)
+                return View(newPessoa);
+            
+            newPessoa.Id = pessoaList.Max(p => p.Id) + 1;
+            pessoaList.Add(newPessoa);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
